f4b6a48de63a09b79a027f49cc548afe5ab28081
Fausto Sánchez Hoya <faustosanchezhoya@gmail.com>
2021-11-22 15:12:23 +0100

    ZombieSeguidor
    Ahora ya hay un zombie que sigue a Mike pero no consigo hacer que no acelere o que se puedan poner más zombies

--- Assets/DangerousDungeons/GeneralScripts/Enemigos/EnemyController.cs
+++ Assets/DangerousDungeons/GeneralScripts/Enemigos/EnemyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
@@ -9,12 +10,20 @@ public class EnemyController : MonoBehaviour
     private Animator animator;
     private float tiempoMuerto;
     private int vidaAnterior;
+    private GameObject Mike;
+    private float Distancia;
+    public float DistanciaMinima;
+    private float velocidad;
+    public RaycastHit rayo;
+    private NavMeshAgent navMeshAgent;
     void Start()
     {
         //La inicialización de la vida se hace mediante la interfaz ya que los diferentes enemigos tendrán diferentes vidas iniciales
         animator = this.gameObject.GetComponent<Animator>();
         tiempoMuerto = 0;
         vidaAnterior = vida;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        Mike = GameObject.Find("Mike");
     }
 
     private void Update()
@@ -32,6 +41,20 @@ public class EnemyController : MonoBehaviour
         {
             animator.SetTrigger("Golpe");
             vidaAnterior = vida;
+        }else
+        {
+            Vector3 posicionFinal = new Vector3(Mike.transform.position.x + 0.5f, Mike.transform.position.y, Mike.transform.position.z + 0.5f);
+            navMeshAgent.destination = posicionFinal;
+            Debug.Log(navMeshAgent.remainingDistance);
+            if(navMeshAgent.remainingDistance<=0.7)
+            {
+                animator.SetBool("Andar", false);
+            }
+            else
+            {
+                animator.SetBool("Andar", true);
+            }
+               
         }
     }
 }
